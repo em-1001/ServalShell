f1165bfd7dec147992461d1f296831148b02fb28
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-02-24 21:25:58 +0900

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ def get_config():
       "model_basename": "tmodel_",
       "preload": None,
       "tokenizer_file": "tokenizer_{0}.json",
-      "experiment_name": "/content/tmodel"
+      "experiment_name": "/content/drive/MyDrive/tmodel"
   }
 
 
