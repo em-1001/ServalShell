c55c7847818f9c93b5c4ca70a50b194a6be754c0
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-02-25 21:25:50 +0900

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from pathlib import Path
 def get_config():
   return {
       "batch_size": 8,
-      "num_epochs": 20,
+      "num_epochs": 100,
       "lr": 1e-4,
       "seq_len": 350,
       "d_model": 512,
