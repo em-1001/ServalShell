e9d9af633f0a88902cbc55b58a0bb65c95b50237
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-02-29 17:46:23 +0900

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ def get_config():
       "batch_size": 8,
       "num_epochs": 100,
       "lr": 1e-4,
-      "seq_len": 350,
+      "seq_len": 350, # 510
       "d_model": 512,
       "lang_src": "invocation",
       "lang_tgt": "cmd",
@@ -26,6 +26,6 @@ def get_config():
 def get_weights_file_path(config):
   model_folder = config["model_folder"]
   model_basename = config["model_basename"]
-  model_filename = f"{model_basename}x.pth"
+  model_filename = f"{model_basename}x.pth" # x2
   return str(Path('.') / model_folder / model_filename)
 
