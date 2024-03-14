642e15ae94d81e02ff20f278329b1d6797806f04
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-03-14 22:48:36 +0900

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 # preprocess data
+
 import sys
 sys.path.append("./Tellina")
 from bashlint.data_tools import bash_tokenizer, bash_parser, ast2tokens, ast2command
@@ -25,7 +26,6 @@ def pre_processing(config):
     for item in ds_raw.values():
         item[config['lang_src']] = ' '.join(tokenizer.ner_tokenizer(item[config['lang_src']])[0])
         item[config['lang_tgt']] = ' '.join(bash_tokenizer(item[config['lang_tgt']], loose_constraints=True, arg_type_only=True))
-        ...
 
     # save pre-processed data to a new JSON file
     save_data(ds_raw, './Data/nl2bash/preprocessed_data.json')
