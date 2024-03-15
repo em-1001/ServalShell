59fb31d223963c914dfd99dedf256ac456ec5fe5
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-03-15 23:23:42 +0900

    Update config.py

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ def get_config():
       "model_basename": "tmodel_",
       "preload": None,
       "tokenizer_file": "tokenizer_{0}.json",
-      "experiment_name": "./tmodel"
+      "experiment_name": "./tmodel",
+      "cos_anneal": False
   }
 
 
