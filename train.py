64e8ebf8a3d193c721efaab4f5e9086cad4162fe
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-03-20 22:59:20 +0900

    Update train.py

--- train.py
+++ train.py
@@ -33,40 +33,10 @@ from pathlib import Path
 
 import json
 from sklearn.model_selection import train_test_split
+from beam_search import greedy_search, beam_search, length_penalty
 
 
-def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
-    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
-    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
-
-    # Precompute the encoder output and reuse it for every step
-    encoder_output = model.encode(source, source_mask)
-    # Initialize the decoder input with the sos token
-    decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
-    while True:
-        if decoder_input.size(1) == max_len:
-            break
-
-        # build mask for target
-        decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(device)
-
-        # calculate output
-        out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
-
-        # get next token
-        prob = model.project(out[:, -1])
-        _, next_word = torch.max(prob, dim=1)
-        decoder_input = torch.cat(
-            [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1
-        )
-
-        if next_word == eos_idx:
-            break
-
-    return decoder_input.squeeze(0)
-
-
-def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_step, writer, num_examples=2):
+def run_validation(model, config, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_step, writer, num_examples=2):
     model.eval()
     count = 0
 
@@ -93,7 +63,10 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
             assert encoder_input.size(
                 0) == 1, "Batch size must be 1 for validation"
 
-            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+            if config['beam_search']:
+                model_out = beam_search(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device, beam_width=config['beam_width'])
+            else:
+                model_out = greedy_search(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
 
             source_text = batch["src_text"][0]
             target_text = batch["tgt_text"][0]
@@ -261,7 +234,7 @@ def train_model(config):
       scheduler.step()
 
     # Run validation at the end of every epoch
-    run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+    run_validation(model, config, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
 
     # Save the model at the end of every epoch
     model_filename = get_weights_file_path(config)
@@ -272,7 +245,7 @@ def train_model(config):
         'global_step': global_step
     }, model_filename)
 
-    if epoch == 1:# epoch > 0 and epoch % 3 == 0:
+    if epoch > 0 and epoch % 2 == 0:
       nl = 'print current user name'
       nl = ' '.join(tokenizer.ner_tokenizer(nl)[0])
       translate(nl)
