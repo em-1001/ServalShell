8590e99ca9fae17f8c3d0c743c742059e5770bd4
em-1001 <80628552+em-1001@users.noreply.github.com>
2024-03-21 21:22:10 +0900

    Update beam_search.py

--- beam_search.py
+++ beam_search.py
@@ -7,16 +7,16 @@ def length_penalty(length, alpha=1.2, min_length=3):
 
 def beam_search(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device, beam_width=3):
     sos_idx = tokenizer_tgt.token_to_id('[SOS]')
-    eos_idx = tokenizer_tgt.token_to_id('[EOS]')    
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
 
     # Precompute the encoder output and reuse it for every step
     encoder_output = model.encode(source, source_mask)
 
     # Initialize beams
     beams = [[torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device), 0.0]]
-    eos_cnt = 0 
+    eos_cnt = 0
     eos_candidates = []
-    
+
     for _ in range(max_len):
         next_candidates = []
 
@@ -30,7 +30,7 @@ def beam_search(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_le
 
             # get top k next words
             prob = model.project(out[:, -1])
-            topk_scores, topk_words = torch.topk(prob, 2*beam_width-1) 
+            topk_scores, topk_words = torch.topk(prob, 2*beam_width-1)
 
             boundary = beam_width
             loop = 0
@@ -69,8 +69,8 @@ def beam_search(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_le
     #for text, score in eos_candidates:
     #    print(text, score, len(text[0]), score/length_penalty(len(text[0])))
 
-    best_beam = min(eos_candidates, key=lambda x: x[1]/length_penalty(len(x[0][0])))
-    return best_beam[0]
+    best_beam = sorted(eos_candidates, key=lambda x: x[1]/length_penalty(len(x[0][0])))
+    return [best_beam[i][0] for i in range(beam_width)]
 
 
 
